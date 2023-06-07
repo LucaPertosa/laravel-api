8e4b4c4bb7fbfc1774e94d209c48c424b946beb1
LucaPertosa <luca.pertosa@hotmail.it>
2023-06-07 16:51:25 +0200

    Created Type Model & added function in type and project model to specify relation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,10 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'description'];
+
+    // crea relazione con tabella type
+    public function type()
+    {
+		return $this->belongsTo(Type::class);
+	}
 }
