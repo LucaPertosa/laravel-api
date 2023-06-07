627485a7672e4236f2dcfe7607f098c240976237
LucaPertosa <luca.pertosa@hotmail.it>
2023-06-07 17:28:21 +0200

    index.blade refactoring: added type row in table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,7 +14,8 @@
                 <th scope="col">Id</th>
                 <th scope="col">Titolo</th>
                 <th scope="col">Slug</th>
-                <th scope="col">Actions</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">Azioni</th>
             </tr>
         </thead>
         <tbody>
@@ -23,6 +24,7 @@
                     <th scope="row">{{ $project->id }}</th>
                     <td>{{ $project->title }}</td>
                     <td>{{ $project->slug }}</td>
+                    <td>{{ $project->type?->name ? $project->type?->name : 'non assegnata' }}</td>
                     <td>
                         <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
                             <i class="fa-solid fa-eye"></i>
