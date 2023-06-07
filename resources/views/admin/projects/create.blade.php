950bcf28fe8dad9b7dffa085b7c6c4b3e7d9412d
LucaPertosa <luca.pertosa@hotmail.it>
2023-06-07 19:02:35 +0200

    Add: types form

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,6 +17,21 @@
         </div>
 
         <div class="mb-3">
+            <label for="type" class="form-label">Titolo</label>
+            <select class="form-select" name="type_id" id="type">
+                <option value=""></option>
+                @foreach ($types as $type)
+                    <option @selected(old('type_id') == $type->id) value="{{$type->id}}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">{{old('description')}}</textarea>
             @error('description')
